3e9a8e26cedbfbfe2183a9d03e7f16f3bf75133c
Vesta <hotabych39@gmail.com>
2026-03-02 11:11:27 +0300

    fix: set correct base path and add 404 fallback for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/kinoflow/'),
   routes,
   scrollBehavior() {
     return { top: 0 }
